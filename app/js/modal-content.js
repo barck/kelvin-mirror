709d5f412a04258fd60e3c38f3e445dbfeeead7d
barck94 <artiv09@gmail.com>
2017-09-11 18:16:17 +0500

    modal block js added

---
+++ app/js/modal-content.js
@@ -0,0 +1,24 @@
+var link = document.querySelector(".btn-hero");
+var popup = document.querySelector(".modal-content");
+var close = document.querySelector(".modal-content-close");
+var overlay = document.querySelector(".modal-overlay");
+
+
+link.addEventListener("click", function(event){
+    event.preventDefault();
+    popup.classList.add("modal-content-show");
+    overlay.classList.add("modal-overlay-show");
+});
+
+overlay.addEventListener("click", function(event){
+    event.preventDefault();
+    popup.classList.remove("modal-content-show");
+    overlay.classList.remove("modal-overlay-show");
+});
+
+close.addEventListener("click", function(event){
+    event.preventDefault();
+    popup.classList.remove("modal-content-show");
+    overlay.classList.remove("modal-overlay-show");
+});
+
--- public/js/scripts.js
+++ public/js/scripts.js
@@ -1,3 +1,28 @@
+var link = document.querySelector(".btn-hero");
+var popup = document.querySelector(".modal-content");
+var close = document.querySelector(".modal-content-close");
+var overlay = document.querySelector(".modal-overlay");
+
+
+link.addEventListener("click", function(event){
+    event.preventDefault();
+    popup.classList.add("modal-content-show");
+    overlay.classList.add("modal-overlay-show");
+});
+
+overlay.addEventListener("click", function(event){
+    event.preventDefault();
+    popup.classList.remove("modal-content-show");
+    overlay.classList.remove("modal-overlay-show");
+});
+
+close.addEventListener("click", function(event){
+    event.preventDefault();
+    popup.classList.remove("modal-content-show");
+    overlay.classList.remove("modal-overlay-show");
+});
+
+
 /*
     jQuery Masked Input Plugin
     Copyright (c) 2007 - 2015 Josh Bush (digitalbush.com)
